c3446d4277b7f6b3f53187588178f10c46b393c6
Matt Dainty <matt@bodgit-n-scarper.com>
2018-03-19 16:19:56 -0700

    Perform TSIG on resource reads
    Rather than copy and paste the same code, refactor DNS query code out
    into separate function and while there, handle UDP message truncation
    by first retrying with EDNS0 and then TCP.

--- dns/resource_dns_aaaa_record_set_test.go
+++ dns/resource_dns_aaaa_record_set_test.go
@@ -3,7 +3,6 @@ package dns
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -17,10 +16,6 @@ func TestAccDnsAAAARecordSet_basic(t *testing.T) {
 
 	deleteAAAARecordSet := func() {
 		meta := testAccProvider.Meta()
-		c := meta.(*DNSClient).c
-		srv_addr := meta.(*DNSClient).srv_addr
-		keyname := meta.(*DNSClient).keyname
-		keyalgo := meta.(*DNSClient).keyalgo
 
 		msg := new(dns.Msg)
 
@@ -31,11 +26,7 @@ func TestAccDnsAAAARecordSet_basic(t *testing.T) {
 		rr_remove, _ := dns.NewRR(fmt.Sprintf("%s 0 AAAA", rec_fqdn))
 		msg.RemoveRRset([]dns.RR{rr_remove})
 
-		if keyname != "" {
-			msg.SetTsig(keyname, keyalgo, 300, time.Now().Unix())
-		}
-
-		r, _, err := c.Exchange(msg, srv_addr)
+		r, err := exchange(msg, true, meta)
 		if err != nil {
 			t.Fatalf("Error deleting DNS record: %s", err)
 		}
@@ -84,8 +75,6 @@ func TestAccDnsAAAARecordSet_basic(t *testing.T) {
 
 func testAccCheckDnsAAAARecordSetDestroy(s *terraform.State) error {
 	meta := testAccProvider.Meta()
-	c := meta.(*DNSClient).c
-	srv_addr := meta.(*DNSClient).srv_addr
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "dns_aaaa_record_set" {
 			continue
@@ -102,7 +91,7 @@ func testAccCheckDnsAAAARecordSetDestroy(s *terraform.State) error {
 
 		msg := new(dns.Msg)
 		msg.SetQuestion(rec_fqdn, dns.TypeAAAA)
-		r, _, err := c.Exchange(msg, srv_addr)
+		r, err := exchange(msg, false, meta)
 		if err != nil {
 			return fmt.Errorf("Error querying DNS record: %s", err)
 		}
@@ -134,12 +123,10 @@ func testAccCheckDnsAAAARecordSetExists(t *testing.T, n string, addr []interface
 		rec_fqdn := fmt.Sprintf("%s.%s", *rec_name, *rec_zone)
 
 		meta := testAccProvider.Meta()
-		c := meta.(*DNSClient).c
-		srv_addr := meta.(*DNSClient).srv_addr
 
 		msg := new(dns.Msg)
 		msg.SetQuestion(rec_fqdn, dns.TypeAAAA)
-		r, _, err := c.Exchange(msg, srv_addr)
+		r, err := exchange(msg, false, meta)
 		if err != nil {
 			return fmt.Errorf("Error querying DNS record: %s", err)
 		}
