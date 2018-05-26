023b7a11111875619ddbe8ae3ba9b8f32611fa07
kt <kt@katbyte.me>
2018-05-25 17:20:15 -0700

    Add acc tests to travis with one broken to test

--- dns/data_dns_cname_record_set_test.go
+++ dns/data_dns_cname_record_set_test.go
@@ -18,7 +18,7 @@ func TestAccDataDnsCnameRecordSet_Basic(t *testing.T) {
 			  host = "www.hashicorp.com"
 			}
 			`,
-			"hashicorp.netlifyglobalcdn.com.",
+			"1hashicorp.netlifyglobalcdn.com.",
 			"www.hashicorp.com",
 		},
 	}
