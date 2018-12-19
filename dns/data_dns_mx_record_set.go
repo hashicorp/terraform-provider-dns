d0e3e080fb7890608ea2ff3f3cc2bd1dd7ce2b5e
csoukup <csoukup33@gmail.com>
2018-12-19 14:30:30 -0600

    sorted mx results for consistent output

--- dns/data_dns_mx_record_set.go
+++ dns/data_dns_mx_record_set.go
@@ -3,6 +3,7 @@ package dns
 import (
 	"fmt"
 	"net"
+	"sort"
 
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -20,6 +21,11 @@ func dataSourceDnsMXRecordSet() *schema.Resource {
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
 			},
+			"priorities": &schema.Schema{
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+				Computed: true,
+			},
 		},
 	}
 }
@@ -32,15 +38,33 @@ func dataSourceDnsMXRecordSetRead(d *schema.ResourceData, meta interface{}) erro
 		return fmt.Errorf("error looking up MX records for %q: %s", zone, err)
 	}
 
-	mxservers := make([]string, len(mxRecords))
-	for i, record := range mxRecords {
-		mxservers[i] = record.Host
+	m := make(map[string]int)
+	for _, record := range mxRecords {
+		m[record.Host] = int(record.Pref)
+	}
+	//List of mxservers sorted by name, not priority
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	mxservers := make([]string, 0, len(m))
+	priorities := make([]int, 0, len(m))
+	for _, k := range keys {
+		mxservers = append(mxservers, k)
+		priorities = append(priorities, m[k])
 	}
 
 	err = d.Set("mxservers", mxservers)
 	if err != nil {
 		return err
 	}
+
+	err = d.Set("priorities", priorities)
+	if err != nil {
+		return err
+	}
+
 	d.SetId(zone)
 
 	return nil
--- dns/data_dns_mx_record_set_test.go
+++ dns/data_dns_mx_record_set_test.go
@@ -12,6 +12,7 @@ func TestAccDataDnsMXRecordSet_Basic(t *testing.T) {
 		DataSourceBlock string
 		DataSourceName  string
 		Expected        []string
+		Priority        []int
 		Zone            string
 	}{
 		{
@@ -31,7 +32,46 @@ func TestAccDataDnsMXRecordSet_Basic(t *testing.T) {
 				"eforward4.registrar-servers.com.",
 				"eforward5.registrar-servers.com.",
 			},
+			[]int{
+				// These results may change if hashicorp.net changes MX host priorities, hosts, or providers.
+				// If you suspect the expected results have changed here, confirm
+				// with e.g. dig hashicorp.net MX +short
+				10,
+				10,
+				10,
+				15,
+				20,
+			},
 			"hashicorp.net",
+		},
+		{
+			`
+			data "dns_mx_record_set" "foo" {
+			zone = "google.com"
+			}
+			`,
+			"foo",
+			[]string{
+				// These results may change if google.com changes MX hosts or providers.
+				// If you suspect the expected results have changed here, confirm
+				// with e.g. dig google.com MX +short
+				"alt1.aspmx.l.google.com.",
+				"alt2.aspmx.l.google.com.",
+				"alt3.aspmx.l.google.com.",
+				"alt4.aspmx.l.google.com.",
+				"aspmx.l.google.com.",
+			},
+			[]int{
+				// These results may change if hashicorp.net changes MX host priorities, hosts, or providers.
+				// If you suspect the expected results have changed here, confirm
+				// with e.g. dig hashicorp.net MX +short
+				20,
+				30,
+				40,
+				50,
+				10,
+			},
+			"google.com",
 		},
 	}
 
@@ -45,6 +85,12 @@ func TestAccDataDnsMXRecordSet_Basic(t *testing.T) {
 					Config: test.DataSourceBlock,
 					Check: resource.ComposeTestCheckFunc(
 						testCheckAttrStringArray(recordName, "mxservers", test.Expected),
+					),
+				},
+				{
+					Config: test.DataSourceBlock,
+					Check: resource.ComposeTestCheckFunc(
+						testCheckAttrIntArray(recordName, "priorities", test.Priority),
 					),
 				},
 				{
