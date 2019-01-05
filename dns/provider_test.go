fb2cbaf2d95655d6e2976e5b8b7641afd2701752
Matt Dainty <matt@bodgit-n-scarper.com>
2019-01-05 16:33:56 +0000

    Add testAccCheckDnsDestroy() helper
    Each resource *Destroy() test function is the same apart from the TF
    resource name and the DNS RR type so create a common function that takes
    these as an argument.

--- dns/provider_test.go
+++ dns/provider_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/miekg/dns"
 )
 
 var testAccProviders map[string]terraform.ResourceProvider
@@ -43,4 +44,29 @@ func testResourceFQDN(name, zone string) string {
 	}
 
 	return fqdn
+}
+
+func testAccCheckDnsDestroy(s *terraform.State, resourceType string, rrType uint16) error {
+	meta := testAccProvider.Meta()
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != resourceType {
+			continue
+		}
+
+		fqdn := testResourceFQDN(rs.Primary.Attributes["name"], rs.Primary.Attributes["zone"])
+
+		msg := new(dns.Msg)
+		msg.SetQuestion(fqdn, rrType)
+		r, err := exchange(msg, false, meta)
+		if err != nil {
+			return fmt.Errorf("Error querying DNS record: %s", err)
+		}
+		// Should either get an NXDOMAIN or a NOERROR and no answers
+		// (and usually an authority record)
+		if !(r.Rcode == dns.RcodeNameError || (r.Rcode == dns.RcodeSuccess && ((rrType == dns.TypeNS && len(r.Ns) == 0) || len(r.Answer) == 0))) {
+			return fmt.Errorf("DNS record still exists: %v (%s)", r.Rcode, dns.RcodeToString[r.Rcode])
+		}
+	}
+
+	return nil
 }
