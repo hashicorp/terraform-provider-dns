5f10f456275546c0856c105c2ab4617834c3b5b0
Alex Pilon <apilon@hashicorp.com>
2019-07-23 21:08:35 -0400

    provide public srv record for datasource test

--- dns/data_dns_srv_record_set_test.go
+++ dns/data_dns_srv_record_set_test.go
@@ -11,23 +11,19 @@ func TestAccDataDnsSRVRecordSet_Basic(t *testing.T) {
 	tests := []struct {
 		DataSourceBlock string
 		DataSourceName  string
-		Expected        []string
-		Host            string
+		ExpectedTarget  string
+		Service         string
 	}{
-		/* FIXME Need an online SRV record
 		{
 			`
 			data "dns_srv_record_set" "srv" {
-			  host = "example.com"
+			  service = "_http._tcp.mxtoolbox.com"
 			}
 			`,
 			"srv",
-			[]string{
-				"2606:2800:220:1:248:1893:25c8:1946",
-			},
-			"example.com",
+			"mxtoolbox.com",
+			"_http._tcp.mxtoolbox.com",
 		},
-		*/
 	}
 
 	for _, test := range tests {
@@ -39,13 +35,13 @@ func TestAccDataDnsSRVRecordSet_Basic(t *testing.T) {
 				{
 					Config: test.DataSourceBlock,
 					Check: resource.ComposeTestCheckFunc(
-						testCheckAttrStringArray(recordName, "srv", test.Expected),
+						resource.TestCheckResourceAttr(recordName, "srv.0.target", test.ExpectedTarget),
 					),
 				},
 				{
 					Config: test.DataSourceBlock,
 					Check: resource.ComposeTestCheckFunc(
-						resource.TestCheckResourceAttr(recordName, "id", test.Host),
+						resource.TestCheckResourceAttr(recordName, "id", test.Service),
 					),
 				},
 			},
