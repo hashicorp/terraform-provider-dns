3c900f81d450630033af1741a9ab912ecbc00aa9
Sean OMeara <sean@sean.io>
2020-07-26 17:01:29 +0200

    adding missing txt records

--- dns/data_dns_txt_record_set_test.go
+++ dns/data_dns_txt_record_set_test.go
@@ -25,6 +25,8 @@ func TestAccDataDnsTxtRecordSet_Basic(t *testing.T) {
 				"google-site-verification=LQZvxDzrGE-ZLudDpkpj-gcXN-5yF7Z6C-4Rljs3I_Q",
 				"google-site-verification=8d7FpfB8aOEYAIkoaVKxg7Ibj438CEypjZTH424Pews",
 				"google-site-verification=y974ACvos30pN7_OBgEZb_byZV8qYtK0G6WZfE7OX8s",
+				"google-site-verification=9D7erI6Bfd9EOHKSIXRe0XQaqAFAjToBtZmyYRzMm34",
+				"keybase-site-verification=5HKqMvJnTWpe8W-Aa8r0y3wuy1bhQ6LwcjaxKE9BOQU",
 			},
 			"terraform.io",
 		},
