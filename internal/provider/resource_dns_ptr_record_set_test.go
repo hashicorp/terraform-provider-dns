be9cbba94409c04378f23b7f3a871a34969f2c79
Joel Verezhak <j.verezhak@gmail.com>
2021-11-22 23:42:45 +0100

    dns_ptr_record_set tests

--- internal/provider/resource_dns_ptr_record_set_test.go
+++ internal/provider/resource_dns_ptr_record_set_test.go
@@ -5,17 +5,18 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/miekg/dns"
 )
 
-func TestAccDnsPtrRecord_basic(t *testing.T) {
+func TestAccDnsPtrRecordSet_basic(t *testing.T) {
 
 	var rec_name, rec_zone string
-	resourceName := "dns_ptr_record.foo"
-	resourceRoot := "dns_ptr_record.root"
+	resourceName := "dns_ptr_record_set.foo"
+	resourceRoot := "dns_ptr_record_set.root"
 
-	deletePtrRecord := func() {
+	deletePtrRecordSet := func() {
 		meta := testAccProvider.Meta()
 
 		msg := new(dns.Msg)
@@ -39,25 +40,28 @@ func TestAccDnsPtrRecord_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckDnsPtrRecordDestroy,
+		CheckDestroy: testAccCheckDnsPtrRecordSetDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDnsPtrRecord_basic,
+				Config: testAccDnsPtrRecordSet_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDnsPtrRecordExists(t, resourceName, "bar.example.com.", &rec_name, &rec_zone),
+					resource.TestCheckResourceAttr(resourceName, "ptrs.#", "1"),
+					testAccCheckDnsPtrRecordSetExists(t, resourceName, []interface{}{"bar.example.com."}, &rec_name, &rec_zone),
 				),
 			},
 			{
-				Config: testAccDnsPtrRecord_update,
+				Config: testAccDnsPtrRecordSet_update,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDnsPtrRecordExists(t, resourceName, "baz.example.com.", &rec_name, &rec_zone),
+					resource.TestCheckResourceAttr(resourceName, "ptrs.#", "2"),
+					testAccCheckDnsPtrRecordSetExists(t, resourceName, []interface{}{"bar.example.com.", "baz.example.com."}, &rec_name, &rec_zone),
 				),
 			},
 			{
-				PreConfig: deletePtrRecord,
-				Config:    testAccDnsPtrRecord_update,
+				PreConfig: deletePtrRecordSet,
+				Config:    testAccDnsPtrRecordSet_update,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDnsPtrRecordExists(t, resourceName, "baz.example.com.", &rec_name, &rec_zone),
+					resource.TestCheckResourceAttr(resourceName, "addresses.#", "2"),
+					testAccCheckDnsPtrRecordSetExists(t, resourceName, []interface{}{"bar.example.com.", "baz.example.com"}, &rec_name, &rec_zone),
 				),
 			},
 			{
@@ -66,9 +70,10 @@ func TestAccDnsPtrRecord_basic(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccDnsPtrRecord_root,
+				Config: testAccDnsPtrRecordSet_root,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDnsPtrRecordExists(t, resourceRoot, "baz.example.com.", &rec_name, &rec_zone),
+					resource.TestCheckResourceAttr(resourceRoot, "addresses.#", "3"),
+					testAccCheckDnsPtrRecordSetExists(t, resourceRoot, []interface{}{"machine1.example.com.", "machine2.example.com.", "machine3.example.com."}, &rec_name, &rec_zone),
 				),
 			},
 			{
@@ -80,11 +85,11 @@ func TestAccDnsPtrRecord_basic(t *testing.T) {
 	})
 }
 
-func testAccCheckDnsPtrRecordDestroy(s *terraform.State) error {
-	return testAccCheckDnsDestroy(s, "dns_ptr_record", dns.TypePTR)
+func testAccCheckDnsPtrRecordSetDestroy(s *terraform.State) error {
+	return testAccCheckDnsDestroy(s, "dns_ptr_record_set", dns.TypePTR)
 }
 
-func testAccCheckDnsPtrRecordExists(t *testing.T, n string, expected string, rec_name, rec_zone *string) resource.TestCheckFunc {
+func testAccCheckDnsPtrRecordSetExists(t *testing.T, n string, pointers []interface{}, rec_name, rec_zone *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -103,6 +108,7 @@ func testAccCheckDnsPtrRecordExists(t *testing.T, n string, expected string, rec
 
 		msg := new(dns.Msg)
 		msg.SetQuestion(rec_fqdn, dns.TypePTR)
+
 		r, err := exchange(msg, false, meta)
 		if err != nil {
 			return fmt.Errorf("Error querying DNS record: %s", err)
@@ -111,40 +117,50 @@ func testAccCheckDnsPtrRecordExists(t *testing.T, n string, expected string, rec
 			return fmt.Errorf("Error querying DNS record")
 		}
 
-		if len(r.Answer) > 1 {
-			return fmt.Errorf("Error querying DNS record: multiple responses received")
+		ptrs := schema.NewSet(schema.HashString, nil)
+		expected := schema.NewSet(schema.HashString, pointers)
+		for _, record := range r.Answer {
+			ptr, _, err := getPtrVal(record)
+			if err != nil {
+				return fmt.Errorf("Error querying DNS record: %s", err)
+			}
+			ptrs.Add(ptr)
 		}
-		record := r.Answer[0]
-		ptr, _, err := getPtrVal(record)
-		if err != nil {
-			return fmt.Errorf("Error querying DNS record: %s", err)
-		}
-		if expected != ptr {
-			return fmt.Errorf("DNS record differs: expected %v, found %v", expected, ptr)
+		if !ptrs.Equal(expected) {
+			return fmt.Errorf("DNS record differs: expected %v, found %v", expected, addresses)
 		}
 		return nil
 	}
 }
 
-var testAccDnsPtrRecord_basic = fmt.Sprintf(`
-  resource "dns_ptr_record" "foo" {
+var testAccDnsPtrRecordSet_basic = fmt.Sprintf(`
+  resource "dns_ptr_record_set" "foo" {
     zone = "example.com."
     name = "r._dns-sd._udp"
-    ptr = "bar.example.com."
+    ptrs = [
+			"bar.example.com.",
+		]
     ttl = 300
   }`)
 
-var testAccDnsPtrRecord_update = fmt.Sprintf(`
-  resource "dns_ptr_record" "foo" {
+var testAccDnsPtrRecordSet_update = fmt.Sprintf(`
+  resource "dns_ptr_record_set" "foo" {
     zone = "example.com."
     name = "r._dns-sd._udp"
-    ptr = "baz.example.com."
+    ptrs = [
+			"bar.example.com.",
+			"baz.example.com.",
+		]
     ttl = 300
   }`)
 
-var testAccDnsPtrRecord_root = fmt.Sprintf(`
-  resource "dns_ptr_record" "root" {
+var testAccDnsPtrRecordSet_root = fmt.Sprintf(`
+  resource "dns_ptr_record_set" "root" {
     zone = "example.com."
-    ptr = "baz.example.com."
+    ptrs = [
+			"machine1.example.com.",
+			"machine2.example.com.",
+			"machine3.example.com.",
+		]
     ttl = 300
   }`)
