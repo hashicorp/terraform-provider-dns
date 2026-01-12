7e9432c3c06f323477cacadab0dbaa60961e9621
Baraa Basata <bbasata@gmail.com>
2026-01-12 11:24:50 -0500

    Replace words

--- examples/resources/dns_aaaa_record_set/resource.tf
+++ examples/resources/dns_aaaa_record_set/resource.tf
@@ -2,8 +2,8 @@ resource "dns_aaaa_record_set" "www" {
   zone = "example.com."
   name = "www"
   addresses = [
-    "fdd5:e282:43b8:5303:dead:beef:cafe:babe",
-    "fdd5:e282:43b8:5303:cafe:babe:dead:beef",
+    "fdd5:e282:43b8:5303:1234:5678:cafe:9012",
+    "fdd5:e282:43b8:5303:cafe:9012:1234:5678",
   ]
   ttl = 300
 }
